395cd555cbd5adfe91c2de0d44ab290662598a05
hhernandez008 <hhernandez008@gmail.com>
2015-10-14 16:13:33 -0700

    Memory Match v0.5 - Heather Hernandez

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Heather on 10/14/2015.
+ */
+//Variables
+var first_card_clicked = null;
+var second_card_clicked = null;
+var total_possible_matches = 2;
+var match_counter = 0;
+
