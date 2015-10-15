bc88430f9dadbcc0f1bb4a1807a6aa661f21d6fb
hhernandez008 <hhernandez008@gmail.com>
2015-10-15 10:43:41 -0700

    Memory Match v0.5 - Heather Hernandez

--- script.js
+++ script.js
@@ -4,25 +4,70 @@
 /*
     Variables
  */
-var first_card_clicked = null;
-var second_card_clicked = null;
+var first_card_clicked = null; //to be assigned a value through function card_clicked
+var second_card_clicked = null; //to be assigned a value through function card_clicked
 var total_possible_matches = 2;
 var match_counter = 0;
-
-
-$(".back").click(card_clicked());
 
 
 /*
     Functions
  */
 
+//Accessed by clicking .back card
+//Checks if the card clicked is the first or second card
+// 1 param
 function card_clicked(element){
-    if(first_card_clicked == null){
-        $(element).hide();
-        first_card_clicked = $(element).find("img").attr("src");
-        console.log(first_card_clicked);
-        return first_card_clicked;
-    }
+    //Ensure only 2 cards viewed at a time
+    if(first_card_clicked != null && second_card_clicked != null) {
+        //do nothing
+        console.log("User trying to select more than two cards.");
+    } else {
+        //hide the back of card
+        $(element).fadeOut(0);
+        //Check value of first_card_clicked
+        if (first_card_clicked == null) {
+            first_card_clicked = $(element).prev().find("img").attr("src");
+            //remove for final version !!!!!!!!!!!!!!!!
+            console.log(first_card_clicked);
+            return first_card_clicked;
+        } else {
+            second_card_clicked = $(element).prev().find("img").attr("src");
+            //remove for final version!!!!!!!!!!!!!!!!!!
+            console.log(second_card_clicked);
+            card_match(first_card_clicked, second_card_clicked);
+            return second_card_clicked;
+        } // end if else for first card clicked == null
+    } // end if else for first & second card clicked != null
+}// end function card_clicked
 
-}
+//Check if value of first_card_clicked & second_card_clicked is equal
+//2 Params
+function card_match(card1, card2) {
+    if (card1 == card2) {
+        match_counter++;
+        //remove for final version!!!!!!!!!!!!!!!!!!
+        console.log(match_counter);
+        winning(match_counter);
+        return match_counter;
+    } else {
+        $(".back").delay(1200).fadeIn(0);
+        first_card_clicked = null;
+        second_card_clicked = null;
+    }
+    console.log(first_card_clicked + " " + second_card_clicked);
+}// end function card_match
+
+//Checks if total possible matches equals the counter
+// 1 Param
+function winning(counter){
+    if (counter == total_possible_matches){
+        $(".card").hide();
+        // activate the win screen
+        $("#winner").addClass("win");
+        console.log("Winner!");
+    } else{
+        first_card_clicked = null;
+        second_card_clicked = null;
+    }
+}//end function winning
